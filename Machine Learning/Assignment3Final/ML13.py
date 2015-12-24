fbd33790709f6bcd9b89b6e63408bb7509e366db
fossdevil <ashutoshk2401@gmail.com>
2015-12-25 00:58:18 +0530

    SVD subspaces and projection matrices added

--- Machine Learning/Assignment3Final/ML13.py
+++ Machine Learning/Assignment3Final/ML13.py
@@ -8,7 +8,7 @@ def generatePoints(n,d):
     points = [] # Points on the surface
     pointsG = [] # Gaussian points mean = 0 var = 1
     for i in range(0,n):
-        point = np.random.normal(0,1,d);
+        point = np.random.normal(0,1,d); # see this is just multivariate normal distribution with mean 0 and var 1 (cov = 0) hence sigma = identity matrix
         pointsG.append(point);
         p = point**2;
         den = np.sqrt(sum(p));
@@ -58,20 +58,22 @@ def projection(points,subspace,n):
     projPoint = []
     subspacet = np.asarray(subspace);
     subspace = subspacet.T;
+    inv = np.linalg.inv(np.dot(subspacet,subspace));
+    projMat = np.dot(np.dot(subspace,inv),subspacet);
     for i in range(0,n):
-        inv = np.linalg.inv(np.dot(subspacet,subspace));
-        proj = np.dot(np.dot(np.dot(subspace,inv),subspacet),points[i]);
+        proj = np.dot(projMat,points[i]);
         projPoint.append(proj);
-    return projPoint;
+    return projMat,projPoint;
 
 def projectionsub(points,subspace,n,n2):
     projsub = []
     for i in range(0,n2):
-        projsub.append(projection(points,subspace[i],n));
-    return projsub;
+        projMat, projPoint = projection(points,subspace[i],n);
+        projsub.append(projPoint);
+    return projMat,projsub;
 
 def plotProj(points,subspace,n,n2,d,k):
-    projLine = projectionsub(points,subspace,n,n2);
+    projMat, projLine = projectionsub(points,subspace,n,n2);
     for i in range(0,n2):
         plt.figure();
         if (k!=1): 
@@ -82,7 +84,7 @@ def plotProj(points,subspace,n,n2,d,k):
         plt.xlabel("Distances from origin");
         plt.ylabel("Frequencies");
         plt.show();          
-    return projLine;
+    return projMat, projLine;
 
 def meanVar(points,d):
     mean = []
@@ -138,56 +140,79 @@ def main():
     northPole, northPoleG = generatePoints(1,d);
 
     xaxis = [0.15];
-    print("For dimension = 50 and for surface points, we observe that the points inside the band:");
+    print("For dimension = 50 and for surface points, we observe that the no of points inside the band(Bandwidth = 0.15-Theoretical):");
     pointinband = pointCalculator(points,n,d,northPole,1,xaxis,1,0.15);
     print(pointinband[0]);
+    print("See that most of the points are in the band");
+    print"";
     
-    print("For dimension = 50 and for internal points, we observe that the points inside the band:");
+    print("For dimension = 50 and for internal points, we observe that the points inside the band(Bandwidth = 0.15-Theoretical):");
     pointinband = pointCalculator(pointsInternal,n,d,northPole,1,xaxis,1,0.15);
     print(pointinband[0]);
+    print("See that most of the points are in the band");
+    print"";
 
     print("For subspace of dimension 1 we observe the following");
-    print("10 graphs will be opened. Please check and close each graph to see final output");
+    print("10 graphs will be opened. Please check and close each graph to see final output(Distribution of projected points)");
     lines = []
     for i in range(0,10):
         pts,ptsG = generatePoints(1,d);
         lines.append(np.asarray(pts));
-    projectionpts = plotProj(pointsG,lines,n,10,d,1); # Gaussian points
+    projMat, projectionpts = plotProj(pointsG,lines,n,10,d,1); # Gaussian points
+    meant = np.dot(projMat, np.zeros(d));
+    covt = np.dot(projMat,np.eye(d));
     mean,cov = meanVar(projectionpts,10);
 
     print("Open file Solution1.txt for solution");
                     
     filename = "Solution1.txt";
     target = open(filename,'w');
-    target.write("The Following are the alpha values:\n");
+    target.write("The Following are the alpha values for surface points:\n");
     # Lists down all the bounds/alphas obtained.
     for i in range(0,d):
             target.write(str(alpha[i]));
             target.write("\n");
 
     target.write("\n");
+    
+    target.write("The Following are the alpha values for Internal points:\n");
+    # Lists down all the bounds/alphas obtained.
+    for i in range(0,d):
+            target.write(str(alpha2[i]));
+            target.write("\n");
+
+    target.write("\n");
 
     for i in range(0,10):
-         target.write("Mean of projection for " + str(i+1));
+         target.write("mean of projection for " + str(i+1));
          target.write("\n");
          target.write(str(mean[i]));
          target.write("\n");
-         target.write("Covariance Matrix for " + str(i+1));
+         target.write("distance from the theoretical value:");
+         target.write(str(np.linalg.norm(mean[i]-meant)));
+         target.write("\n\n");
+         target.write("covariance matrix for " + str(i+1));
          target.write("\n");
          target.write(str(cov[i]));
+         target.write("distance from the theoretical value:");
+         target.write(str(np.linalg.norm(cov[i]-covt)));
          target.write("\n");
          target.write("\n");
 
     target.close();
 
     print("For subspace of dimension 5 we observe the following");
-    print("10 graphs will be opened. Please check and close each graph to see final output");
+    print("10 graphs will be opened. Please check and close each graph to see final output(Distribution of projected points)");
     # Randomly generated 5 dimensional subspace
     subspace = []
     for i in range(0,10):
         pts,ptsG = generatePoints(5,d);
-        subspace.append(np.asarray(pts));
-    projectionpts = plotProj(pointsG,subspace,n,10,d,5); # Gaussian Points
+        U,s,V = np.linalg.svd(np.asarray(pts),full_matrices=True); # Performing an SVD to get subspace
+        V = V[:5,:];
+        subspace.append(V);
+    projMat, projectionpts = plotProj(pointsG,subspace,n,10,d,5); # Gaussian Points
+    meant = np.dot(projMat,np.zeros(d));
+    covt = np.dot(np.dot(projMat,np.eye(d)),projMat);
     mean,cov = meanVar(projectionpts,10);
 
     print("Open file Solution3.txt for solution");
@@ -195,16 +220,21 @@ def main():
     filename = "Solution3.txt"
     target = open(filename,'w');
     for i in range(0,10):
-         target.write("Mean of projection for " + str(i+1));
+         target.write("mean of projection for " + str(i+1));
          target.write("\n");
          target.write(str(mean[i]));
          target.write("\n");
-         target.write("Covariance Matrix for " + str(i+1));
+         target.write("distance from the theoretical value:");
+         target.write(str(np.linalg.norm(mean[i]-meant)));
+         target.write("\n\n");
+         target.write("covariance matrix for " + str(i+1));
          target.write("\n");
          target.write(str(cov[i]));
+         target.write("distance from the theoretical value:");
+         target.write(str(np.linalg.norm(cov[i]-covt)));
          target.write("\n");
-         target.write("\n");
-
+         target.write("\n"); 
+    
     target.close();
 
 if __name__ == "__main__":
